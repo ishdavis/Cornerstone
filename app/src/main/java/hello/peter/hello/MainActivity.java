0e50885ed8225cdd381bb80650d5aaf705309f2a
unknown <peterschaldenbrand@gmail.com>
2015-11-18 16:32:53 -0500

    dp_in_m3_pt2

---
+++ app/src/main/java/hello/peter/hello/MainActivity.java
@@ -0,0 +1,169 @@
+package hello.peter.hello;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView mainTextView;
+    Button mainButton;
+    Button b1,b2;
+    TextView tx1;
+    EditText ed1,ed2;
+    int counter = 3;
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    SharedPreferences mSharedPreferences;
+    String userName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(Color.RED);
+
+        b1=(Button)findViewById(R.id.button);
+        ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+
+        b2=(Button)findViewById(R.id.button2);
+        tx1=(TextView)findViewById(R.id.textView3);
+        //tx1.setVisibility(View.GONE);
+        tx1.setVisibility(View.VISIBLE);
+        tx1.setBackgroundColor(Color.RED);
+        tx1.setText(Integer.toString(3));
+
+        displayWelcome();
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ed1.getText().toString().equals(mSharedPreferences.getString(PREF_NAME, "")) &&
+
+                        ed2.getText().toString().equals("admin")) {
+                    userName= mSharedPreferences.getString(PREF_NAME, "");
+                    Toast.makeText(getApplicationContext(), "Welcome "+userName, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0) {
+                        b1.setEnabled(false);
+                    }
+                }
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+    public void displayWelcome() {
+
+        // Access the device's key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        // Read the user's name,
+        // or an empty string if nothing found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+
+            // If the name is valid, display a Toast welcoming them
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        } else {
+
+        // otherwise, show a dialog to ask for their name
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Hello!");
+        alert.setMessage("What is your name?");
+
+        // Create EditText for entry
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        // Make an "OK" button to save the name
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                // Grab the EditText's input
+                String inputName = input.getText().toString();
+
+                // Put it into memory (don't forget to commit!)
+                SharedPreferences.Editor e = mSharedPreferences.edit();
+                e.putString(PREF_NAME, inputName);
+                e.commit();
+
+                // Welcome the new user
+                Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Make a "Cancel" button
+        // that simply dismisses the alert
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int whichButton) {}
+        });
+
+        alert.show();
+    }
+    }
+}
